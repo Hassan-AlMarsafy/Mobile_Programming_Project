fe2f087351e2b2398848497c5eac38e5d4556852
Hassan-AlMarsafy <hassanelmarsafy@gmail.com>
2025-12-20 01:34:19 +0200

    removed outdated user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,38 +1,3 @@
-class User {
-  int? id;
-  String name;
-  String email;
-  String password;
-  DateTime createdAt;
-
-  User({
-    this.id,
-    required this.name,
-    required this.email,
-    required this.password,
-    required this.createdAt,
-  });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'email': email,
-      'password': password,
-      'created_at': createdAt.toIso8601String(),
-    };
-  }
-
-  factory User.fromMap(Map<String, dynamic> map) {
-    return User(
-      id: map['id'],
-      name: map['name'],
-      email: map['email'],
-      password: map['password'],
-      createdAt: DateTime.parse(map['created_at']),
-    );
-  }
-}
 
 // User Profile model for Firebase
 class UserProfile {
